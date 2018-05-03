6cef93a081a4c61e6efda78dbbdd3a892b00825f
Yuichi Yogo <yuichkun@Yuichis-MacBook-Pro.local>
2018-05-03 13:10:42 +0900

    adjust animations

--- prototypes/prototype-2/loader.js
+++ prototypes/prototype-2/loader.js
@@ -1,12 +1,13 @@
-
 window.addEventListener('load', () => {
     setTimeout(() => {
         const left = document.getElementById('left');
         const right = document.getElementById('right');
         console.log(left);
-        left.classList.add('left-clip');
         left.classList.add('clipper');
-        right.classList.add('right-clip');
         right.classList.add('clipper');
+        setTimeout(() => {
+            left.classList.add('left-clip');
+            right.classList.add('right-clip');
+        }, 50);
     }, 4000);
 });
