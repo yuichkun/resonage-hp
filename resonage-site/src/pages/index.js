4cf80edffd5ef00908f747e9ca8a1acd4e274f5e
Yuichi Yogo <yuichkun@Yuichis-MacBook-Pro.local>
2018-06-07 08:09:51 +0900

    modify wordpress url to resonage.wordpress.com

--- resonage-site/src/pages/index.js
+++ resonage-site/src/pages/index.js
@@ -1,13 +1,32 @@
 import React from 'react'
-import Link from 'gatsby-link'
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+export default ({ data }) => {
+ console.log(data)
+ return (
+   <div>
+     <h1>My WordPress Blog</h1>
+     <h4>Posts</h4>
+     {data.allWordpressPost.edges.map(({ node }) => (
+       <div>
+         <p>{node.title}</p>
+         <div dangerouslySetInnerHTML={{__html: node.excerpt}}/>
+       </div>
+     ))}
+   </div>
+ )
+}
 
-export default IndexPage
+export const pageQuery = graphql`
+ query MyFiles {
+   allWordpressPost(sort: { fields: [date] }) {
+     edges {
+       node {
+         title
+         excerpt
+         slug
+       }
+     }
+   }
+ }
+`
+
