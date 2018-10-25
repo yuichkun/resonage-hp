e96c6cedc7982a2b0de2c87aa2423632b0641dc1
Yuichi Yogo <yuichkun@gmail.com>
2018-10-26 07:32:19 +0900

    setup fontawesome

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,14 @@
 import React from "react"
 import styles from "../styles/Layout.module.scss"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import variables from "../styles/variables.scss"
 
 export default () => (
   <div className={styles.header}>
-    I'm Header!
+    <nav className={styles.mobile}>
+      hello
+      <FontAwesomeIcon icon={faBars} color={variables.ResonageBlue} />
+    </nav>
   </div>
 );
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,6 @@ import React from "react"
 import Header from "./Header"
 import Footer from "./Footer"
 
-
 export default ({ children }) => (
   <div>
     <Header />
