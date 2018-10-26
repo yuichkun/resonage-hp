42470c4d112be9dbe8f153c3ac5bd44613d40f21
Yuichi Yogo <yuichkun@gmail.com>
2018-10-26 16:32:31 +0900

    add logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,17 @@ import styles from "../styles/Layout.module.scss"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import variables from "../styles/variables.scss"
+import Logo from "../../logo.svg";
 
 export default () => (
   <div className={styles.header}>
+   {/* Mobile */}
     <nav className={styles.mobile}>
-      hello
+      <img src={Logo} />
       <FontAwesomeIcon icon={faBars} color={variables.ResonageBlue} />
+    </nav>
+   {/* PC */}
+    <nav>
     </nav>
   </div>
 );
