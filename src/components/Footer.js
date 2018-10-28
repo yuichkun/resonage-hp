27a2213c194e24ed57e0da264306bd02a22b6af6
Yuichi Yogo <yuichkun@gmail.com>
2018-10-28 16:05:40 +0900

    add link to policies

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,14 @@
 import React from "react"
 import styles from "../styles/Layout.module.scss"
+import { Link } from "gatsby"
 
 export default () => (
   <div className={styles.footer}>
+    <div className={styles.top}>
+      <Link to="/policies#social-media-guideline">ソーシャルメディアガイドライン</Link> /
+      <Link to="/policies#privacy-policy">プライバシーポリシー</Link> /
+      <Link to="/policies#site-policy">サイトポリシー</Link>
+    </div>
     Footer!!
   </div>
 );
